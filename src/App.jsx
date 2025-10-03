6c8317b4dae55a1b8cebe19367a279387789baa3
FukcThat <63652e636f6465@gmail.com>
2025-10-03 20:07:18 +0200

    ADD: Global context, react router, tailwindcss, basic pretend data structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import "./App.css";
+import { UseGlobal } from "./context/GlobalContext";
 
-function App() {
-  return <></>;
+function App({ className }) {
+  const { test2 } = UseGlobal();
+  return (
+    <div className={` bg-amber-950 dark:bg-black m-5 hover:m-10 ${className}`}>
+      Hello {test2}!
+    </div>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { GlobalProvider } from "./context/GlobalContext.jsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import ErrorPage from "./components/ErrorPage.jsx";
+import Layout from "./components/Layout.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <GlobalProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<App className={" bg-green-950"} />}></Route>
+            <Route path="*" element={<ErrorPage />}></Route>
+          </Route>
+        </Routes>
+      </GlobalProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
